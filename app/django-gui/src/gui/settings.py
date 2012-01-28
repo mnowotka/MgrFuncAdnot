dcc0a8c7eca79d22aca588cd4ffae2365bb9222f
mnowotka <mmmnow@gmail.com>
2012-01-28 16:40:29 +0100

    domyśle szablony dla rejestracji

--- app/django-gui/src/gui/settings.py
+++ app/django-gui/src/gui/settings.py
@@ -1,5 +1,5 @@
 DATABASE_ENGINE = 'django.db.backends.postgresql_psycopg2'
 DATABASE_NAME = 'ifam'
-INSTALLED_APPS = ['gui', 'django.contrib.auth', 'django.contrib.contenttypes', 'django.contrib.sessions', 'django.contrib.sites', 'django.contrib.messages', 'django.contrib.staticfiles', 'registration']
+INSTALLED_APPS = ['registration_defaults', 'django.contrib.auth', 'django.contrib.contenttypes', 'django.contrib.sessions', 'django.contrib.sites', 'django.contrib.messages', 'django.contrib.staticfiles', 'registration','gui']
 ROOT_URLCONF = ['gui.urls']
 SITE_ID = 1
