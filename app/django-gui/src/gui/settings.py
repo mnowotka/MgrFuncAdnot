28b181f44a6f08911770e755d6e6d5d67bbeccd7
mnowotka <mmmnow@gmail.com>
2012-01-28 15:47:47 +0100

    dodany zalążek rejestracji użytkowników

--- app/django-gui/src/gui/settings.py
+++ app/django-gui/src/gui/settings.py
@@ -1,5 +1,5 @@
 DATABASE_ENGINE = 'django.db.backends.postgresql_psycopg2'
 DATABASE_NAME = 'ifam'
-INSTALLED_APPS = ['gui', 'django.contrib.auth', 'django.contrib.contenttypes', 'django.contrib.sessions', 'django.contrib.sites', 'django.contrib.messages', 'django.contrib.staticfiles']
+INSTALLED_APPS = ['gui', 'django.contrib.auth', 'django.contrib.contenttypes', 'django.contrib.sessions', 'django.contrib.sites', 'django.contrib.messages', 'django.contrib.staticfiles', 'registration']
 ROOT_URLCONF = ['gui.urls']
 SITE_ID = 1
