88e1aa9003306681ea7682235d6e6f1be7da2524
mnowotka <mmmnow@gmail.com>
2012-01-29 14:50:58 +0100

    kosmetyka panelu administratorskiego

--- app/django-gui/src/gui/admin.py
+++ app/django-gui/src/gui/admin.py
@@ -1,7 +1,14 @@
 from gui.models import Task, Subtask, RawResult, InferredResult
 from django.contrib import admin
 
-admin.site.register(Task)
+class SubtaskInline(admin.StackedInline):
+    model = Subtask
+    extra = 1
+
+class TaskAdmin(admin.ModelAdmin):
+    inlines = [SubtaskInline]
+
+admin.site.register(Task, TaskAdmin)
 admin.site.register(Subtask)
 admin.site.register(RawResult)
 admin.site.register(InferredResult)
