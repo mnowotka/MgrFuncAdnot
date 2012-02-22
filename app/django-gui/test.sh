e5a4dd07d7e9d398b01ae9c9648570539336ee27
mnowotka <mmmnow@gmail.com>
2012-02-22 13:48:27 +0100

    dodawania przykładów CD.

--- app/django-gui/test.sh
+++ app/django-gui/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cp src/gui/tests/samples/sample_test_01.fasta src/gui/site_media/files/
-./bin django testserver src/gui/tests/samples/sample_data.json.zip
+./bin/django testserver src/gui/tests/samples/sample_data.json.zip
